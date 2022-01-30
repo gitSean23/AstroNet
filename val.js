d36b00e0592f91a5fedbddce7a769171a926d81d
Sean Feldman <66270571+gitSean23@users.noreply.github.com>
2022-01-29 22:51:02 -0500

    Delete val.js

--- val.js
+++
@@ -1,6 +0,0 @@
-import fetch from 'node-fetch';
-const url = "https://eonet.sci.gsfc.nasa.gov/api/v3/events";
-fetch(url)
-.then(response => response.json())
-.then(data => console.log(data.events[0].title));
-
